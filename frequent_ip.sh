f340a0365d531e3853b0335750b25c52939d017b
Shritej Mayekar <30909435+shritejmayekar@users.noreply.github.com>
2020-04-10 12:05:44 +0530

    Update frequent_ip.sh

--- frequent_ip.sh
+++ frequent_ip.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 cat access.log | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | sort -n | uniq -c | sort -n
 
+#second way tail
+cat  access.log | awk '{print $11}' | sort | uniq -c | sort -nr | tail -n 4
+
+# head
+cat  access.log | awk '{print $11}' | sort | uniq -c | sort -nr | head -n 4
